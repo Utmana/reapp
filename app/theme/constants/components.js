f91209fa04cc522a04f107916535e58a102cf816
irony <cl@iteam.se>
2015-03-15 09:43:09 +0100

    color scheme

--- app/theme/constants/components.js
+++ app/theme/constants/components.js
@@ -1,3 +1,69 @@
+var c = {brandColor: '#d7d3b9'};
+
 module.exports = {
-  // can override reapp-ui/themes/ios/constants/components.js
+// Alert
+  alertBG: c.brandColor,
+  alertColor: '#fff',
+
+  // Badge
+  badgeBG: c.mid,
+  badgeColor: '#fff',
+
+  // Bar
+  barBG: '#f6f6f6',
+  barColor: c.brandColorInactive,
+  barColorActive: c.brandColor,
+  barHeight: '49px',
+  barLineHeight: '49px',
+  barBorderColor: c.light,
+
+  // Button
+  buttonBorderColor: '#fff',
+  buttonColor: '#fff',
+  buttonActiveBG: '#fff',
+  buttonActiveColor: '#fff',
+  buttonFocusedBG: 'rgba(0,0,0,0.1)',
+  buttonColorTitleBar: '#fff',
+
+  // Dots
+  dotBG: '#000',
+
+  // Icon
+  iconColor: c.brandColor,
+  iconColorTitleBar: c.activeBG,
+
+  // List
+  listTitleColor: '#f7f7f7',
+  listBG: '#fff',
+
+  // ListItem
+  listItemBorderColor: c.hairline ? '#bcbbc1' : '#dadbe3',
+  listItemArrowColor: '#b7b8bd',
+  listItemContentColor: '#000',
+  listItemAfterColor: '#7b7b7b',
+  listItemTitleAfterColor: '#7b7b7b',
+
+  // Popover
+  popoverArrowSize: 26,
+  popoverBG: '#fff',
+  popoverItemBorder: '1px solid #ccc',
+  popoverOverlayBG: 'rgba(0,0,0,0.3)',
+
+  // SearchBar
+  searchBarHeight: 44,
+  searchBarBG: '#bdbdc3',
+  searchBarBorderColor: '#a6a6a6',
+
+  // TitleBar
+  // iOS7 add extra padding for statusbar
+  titleBarHeight: (c.ios7 || c.standalone) ? 64 : 44,
+  titleBarPaddingTop: (c.ios7 || c.standalone) ? 20 : 0,
+  titleBarColor: c.black,
+  titleBarBorderColor: c.light,
+  titleBarFontSize: '16px',
+  titleBarBG: '#262729',
+
+  // View
+  viewBG: '#d7d3b9',
+  viewPad: '10px'
 };
--- app/theme/styles/TitleBar.js
+++ app/theme/styles/TitleBar.js
@@ -1,5 +1,35 @@
 module.exports = {
-  self: {
-    // can override reapp-ui/themes/ios/styles/TitleeBar.js
-  }
+    self: {
+    background: '#262729',
+    borderStyle: 'solid',
+    borderWidth: 0,
+    padding: 0,
+    textAlign: 'center',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    fontWeight: 500,
+    zIndex: 2,
+    flexFlow: 'row',
+    WebkitFlexFlow: 'row',
+    justifyContent: 'space-between',
+    WebkitJustifyContent: 'space-between'
+  },
+
+  transparent: {
+    background: 'none',
+    borderBottom: 'none'
+  },
+
+  left: {
+    margin: 'auto 0'
+  },
+
+  mid: {
+    color: '#fff',
+    margin: 'auto 0',
+    position: 'relative',
+    fontWeight: 500
+  },
 };
