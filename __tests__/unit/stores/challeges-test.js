9f658c8e891c7df5bd606f742b668ee9aa530bd4
Radu Achim <radu.achim@iteam.se>
2015-03-20 11:49:26 +0100

    Started adding test and npm test (fails right now)

---
+++ __tests__/unit/stores/challeges-test.js
@@ -0,0 +1,62 @@
+'use strict';
+
+jest.dontMock('object-assign');
+jest.dontMock(`${process.cwd()}/app/stores/challenges`);
+
+describe('/stores/Challenges', function () {
+  var Challenges;
+  var React;
+  var TestUtils;
+  var request;
+  var config;
+
+  beforeEach(function () {
+    config = {
+      BASE_URL: 'http://foo.bar'
+    };
+    Challenges = require(`${process.cwd()}/app/stores/challenges`);
+    request = require(`${process.cwd()}/app/services/request`);
+    React = require('react/addons');
+    TestUtils = React.addons.TestUtils;
+    jest.setMock(`${process.cwd()}/app/constants/config`, config)
+  });
+
+  describe('#get', function () {
+    it('should be defined and a function', function () {
+      expect(Challenges.get).toBeDefined();
+      expect(Challenges.get).toEqual(jasmine.any(Function));
+    });
+    it('makes the correct request', function () {
+      var get = request.get.mock;
+      Challenges.get(1337);
+      expect(get.calls.length).toBe(1);
+      expect(get.calls[0][0]).toEqual('http://foo.bar/challenges/1337');
+    });
+  });
+
+  xdescribe('#getList', function () {
+    it('should be defined and a function', function () {
+      expect(Challenges.getList).toBeDefined();
+      expect(Challenges.getList).toEqual(jasmine.any(Function));
+    });
+    it('makes the correct request', function () {
+      var get = request.get.mock;
+      Challenges.getList(1337);
+      expect(get.calls.length).toBe(1);
+      expect(get.calls[0][0]).toEqual('http://foo.bar/challenges');
+    });
+  });
+
+  xdescribe('#accept', function () {
+    it('should be defined and a function', function () {
+      expect(Challenges.accept).toBeDefined();
+      expect(Challenges.accept).toEqual(jasmine.any(Function));
+    });
+    it('makes the correct request', function () {
+      var post = request.post.mock;
+      Challenges.accept(1337);
+      expect(post.calls.length).toBe(1);
+      expect(post.calls[0][0]).toEqual('http://foo.bar/challenges/1337/accept');
+    });
+  });
+});
