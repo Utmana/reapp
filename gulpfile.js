76397619d7e552dcf2835200f15f22c36a2bcdec
Radu Achim <radu.achim@iteam.se>
2015-03-25 22:14:57 +0100

    Small steps to S3 deploy, work in progress

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,10 @@ var shell = require('gulp-shell');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var cache = require('gulp-cached');
+var s3 = require('gulp-s3');
 var pkg = require('./package');
 var jshintConfig = pkg.jshintConfig;
+
 
 var PATH = {
   HTML: './index.html',
@@ -34,8 +36,21 @@ gulp.task('watch', function () {
   gulp.watch([PATH.SOURCE + '**/*.js', PATH.SOURCE + '**/*.jsx', PATH.TEST + '**/*.js'], ['jshint', 'jest']);
 });
 
+gulp.task('s3', ['jest', 'build'], function () {
+  var awsConfig = {
+    "key": "AKIAI3Z7CUAFHG53DMJA",
+    "secret": "acYxWRu5RRa6CwzQuhdXEfTpbQA+1XQJ7Z1bGTCx",
+    "bucket": "dev.example.com",
+    "region": "eu-west-1"
+  };
+  gulp.src('./build/**')
+    .pipe(s3(awsConfig));
+});
+
 gulp.task('jest', shell.task('node --harmony ./node_modules/.bin/jest'));
 
 gulp.task('test', ['jest', 'watch']);
 
 gulp.task('default', ['run']);
+
+gulp.task('deploy', ['s3']);
